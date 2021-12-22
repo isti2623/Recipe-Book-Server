c9d357d11a007eac6fa2ee99f31d3db1e484eb04
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-12-22 23:08:03 +0600

    update delete

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ async function run() {
             res.send(recipePostReq);
         })
 
+
+        //Blood DELETE Api
+        app.delete('/recipePostReq/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await recipePostReqCollection.deleteOne(query);
+            res.send(result);
+        })
+
         //Recipe POST Api
         app.post('/recipePostReq', async (req, res) => {
             const recipeName = req.body.recipeName;
