37255142959b5258db3c5de89e02776328acd420
isti2623 <istiaqueahmed2019.bd@gmail.com>
2022-01-04 00:56:34 +0600

    update

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(fileUpload({ limit: '50mb' }));
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sovrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cenr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 
